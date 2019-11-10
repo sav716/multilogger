38d2670c30c735670335e7bc09d197cf363bd1a2
ADMIN_Ryan <ryanlee2830@gmail.com>
2019-11-09 20:11:40 -0800

    Added costomized 404 and 500 error pages

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'learning_log/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'PST'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
