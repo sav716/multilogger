dd2366d7661c21cf61fc98d219f37c227e4fc73b
ADMIN_Ryan <ryanlee2830@gmail.com>
2019-11-09 20:15:55 -0800

    use get_object_or_404 to route mishandled requests (most likely a user requested a topic from another users account)

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
@@ -20,7 +20,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """Show a single topic and all it's entries"""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     # Stops direct url requests from hacking into other user accounts
     if topic.owner != request.user:
         raise Http404
